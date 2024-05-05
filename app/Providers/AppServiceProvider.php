efacc8183bbc2d6fee0568506822c3a20607c356
kyawkyawhtet <kyaw.kyaw.htet@flymya.co>
2024-05-05 16:35:42 +0700

    fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (env('APP_ENV') !== 'local') { //so you can work on it locally
+            $url->forceScheme('https');
+        }
     }
 }
