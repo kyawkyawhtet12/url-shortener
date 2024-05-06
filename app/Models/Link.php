280758360e3812bcb1a4c277c12672d53f96311c
kyawkyawhtet <kyaw.kyaw.htet@flymya.co>
2024-05-06 14:40:31 +0700

    upate

--- app/Models/Link.php
+++ app/Models/Link.php
@@ -32,7 +32,18 @@ class Link extends Model
 
     public static function setLinkAttribute($value)
     {
-        $urlFormatter = new \UrlFormatter();
-        return $formattedUrl = $urlFormatter->format($value);
+        return $formattedUrl = self::format($value);
+    }
+
+    private static function format($value)
+    {
+        // Check if the link doesn't already start with 'http://' or 'https://'
+        if (!preg_match("~^(?:f|ht)tps?://~i", $value)) {
+            // If not, prepend 'https://' to the link
+            return 'https://' . $value;
+        } else {
+            // Otherwise, use the link as it is
+            return $value;
+        }
     }
 }
