e46c5b03e032276237649537df341e8d0792bdc4
kyawkyawhtet <kyaw.kyaw.htet@flymya.co>
2024-05-10 22:49:21 +0700

    add ads

--- resources/views/url_shortener.blade.php
+++ resources/views/url_shortener.blade.php
@@ -9,6 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <script src="https://richinfo.co/richpartners/in-page/js/richads-ob.js?pubid=924639&siteid=346235" async></script>
 </head>
 
 <body>
