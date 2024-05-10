f4e42afd943440a8903be43865a7f40696eec57a
kyawkyawhtet <kyaw.kyaw.htet@flymya.co>
2024-05-10 13:12:17 +0700

    update homepage

--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,12 @@ use \App\Http\Controllers\LinkController;
 
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/urls', function(){
     return view('url_shortener');
 });
+
+// Route::get('/urls', function(){
+//     return view('url_shortener');
+// });
 
 Route::post('/short_url', [LinkController::class, 'shortUrl'])->name('url.short');
 Route::get('/{code}', [LinkController::class, 'redirectUrl'])->name('url.redirect');
