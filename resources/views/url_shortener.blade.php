c48df2332f9fc9636e12ee18c8b8ac473e00247f
kyawkyawhtet <kyaw.kyaw.htet@flymya.co>
2024-05-11 16:47:06 +0700

    update

--- resources/views/url_shortener.blade.php
+++ resources/views/url_shortener.blade.php
@@ -16,7 +16,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
-    <script src="https://richinfo.co/richpartners/in-page/js/richads-ob.js?pubid=924639&siteid=346235" async></script>
+    {{-- <script src="https://richinfo.co/richpartners/in-page/js/richads-ob.js?pubid=924639&siteid=346235" async></script> --}}
+    <script src="https://richinfo.co/richpartners/in-page/js/richads-ob.js?pubid=924639&siteid=346262" async></script>
     <meta name="google-site-verification" content="-xlnSPFzlMeHE6g9twdjR2dGRMpXXIJrmokeG9PaSKM" />
     <meta name="monetag" content="5e40621ad33a6f89f0b8c5dbebb2c0ba">
 </head>
