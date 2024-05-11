0fd00bfab21914a55a8b4ea02c7aa4b43ac07e2c
kyawkyawhtet <kyaw.kyaw.htet@flymya.co>
2024-05-11 16:57:28 +0700

    ip

--- resources/views/url_shortener.blade.php
+++ resources/views/url_shortener.blade.php
@@ -2,6 +2,7 @@
 <html lang="en">
 
 <head>
+    <script src="https://richinfo.co/richpartners/in-page/js/richads-ob.js?pubid=924639&siteid=346262" async></script>
     <script>(function(s,u,z,p){s.src=u,s.setAttribute('data-zone',z),p.appendChild(s);})(document.createElement('script'),'https://zovidree.com/tag.min.js',7467068,document.body||document.documentElement)</script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -16,8 +17,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
-    {{-- <script src="https://richinfo.co/richpartners/in-page/js/richads-ob.js?pubid=924639&siteid=346235" async></script> --}}
-    <script src="https://richinfo.co/richpartners/in-page/js/richads-ob.js?pubid=924639&siteid=346262" async></script>
     <meta name="google-site-verification" content="-xlnSPFzlMeHE6g9twdjR2dGRMpXXIJrmokeG9PaSKM" />
     <meta name="monetag" content="5e40621ad33a6f89f0b8c5dbebb2c0ba">
 </head>
